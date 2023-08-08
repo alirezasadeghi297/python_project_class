74f72a57f8ef0a2017feddc9e4bc7ad144f29364
alirezasadeghi297 <alirezasadeghi297@gmail.com>
2023-08-09 00:13:51 +0330

    add code 2 for repeated items

--- intersection.py
+++ intersection.py
@@ -1,5 +1,9 @@
-list1=[1,2,3,'a','a','b']
-list2=[1,3,'z','b',4]
+'''
+code1
+unique items:
+'''
+list1=[1,2,3,'a','a','ali',3,'alireza']
+list2=[1,3,'z',4,3,'c','alireza','alireza']
 
 def intersection(list1,list2):
     union_item1=set(list1)
@@ -7,4 +11,17 @@ def intersection(list1,list2):
     union_item1.symmetric_difference_update(union_item2)
     return(union_item1)
 
-print(intersection(list1,list2))
+# print(intersection(list1,list2))
+
+'''
+code2
+repeated items
+'''
+def intersection3(list1,list2):
+    temp_list=list1.copy()
+    for item in temp_list:
+        if item in list2:
+            list1.remove(item)
+            list2.remove(item)
+    return list1+list2
+print(intersection3(list1,list2))
